16e620088e716cc412662d00cba1778fd590ca5c
cord@wsl <1810046993@qq.com>
2024-06-23 13:19:49 +0800

    Finish Day3

--- main/main.go
+++ main/main.go
@@ -2,14 +2,28 @@ package main
 
 import (
 	myrpc "MyRPC"
-	"fmt"
 	"log"
 	"net"
 	"sync"
 	"time"
 )
 
+type Foo int
+
+type Args struct{ Num1, Num2 int }
+
+func (f Foo) Sum(args Args, reply *int) error {
+	*reply = args.Num1 + args.Num2
+	return nil
+}
+
 func startServer(addr chan string) {
+  // register service
+	var foo Foo
+	if err := myrpc.Register(&foo); err != nil {
+		log.Fatal("register error:", err)
+	}
+
 	// pick a free port
 	l, err := net.Listen("tcp", ":0")
 	if err != nil {
@@ -21,26 +35,30 @@ func startServer(addr chan string) {
 }
 
 func main() {
-  log.SetFlags(0)
-  addr := make(chan string)
-  go startServer(addr)
-  client, _ := myrpc.Dial("tcp", <-addr)
-  defer func() { _ = client.Close() }()
+	log.SetFlags(0)
+	addr := make(chan string)
+	go startServer(addr)
 
-  time.Sleep(time.Second)
-  // send request & receive response
-  var wg sync.WaitGroup
-  for i := 0; i < 5; i++ {
-    wg.Add(1)
-    go func(i int) {
-      defer wg.Done()
-      args := fmt.Sprintf("geerpc req %d", i)
-      var reply string
-      if err := client.Call("Foo.Sum", args, &reply); err != nil {
-        log.Fatal("call Foo.Sum error:", err)
-      }
-      log.Println("reply:", reply)
-    }(i)
-  }
-  wg.Wait()
+	client, _ := myrpc.Dial("tcp", <-addr)
+	defer func() { 
+    _ = client.Close() 
+  }()
+
+	time.Sleep(time.Second)
+
+	// send request & receive response
+	var wg sync.WaitGroup
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			args := &Args{Num1: i, Num2: i * i}
+			var reply int
+			if err := client.Call("Foo.Sum", args, &reply); err != nil {
+				log.Fatal("call Foo.Sum error:", err)
+			}
+			log.Printf("%d + %d = %d", args.Num1, args.Num2, reply)
+		}(i)
+	}
+	wg.Wait()
 }
